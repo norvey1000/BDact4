04e6214a964a41e3c480dcfb46385b24bc8cb5b2
Andres-Monterrosa <amonterrosa@uniguajira.edo.co>
2021-09-30 16:31:44 -0500

    Relacion cliente

--- concepcionario/apps/cliente/models.py
+++ concepcionario/apps/cliente/models.py
@@ -8,4 +8,11 @@ class Cliente(models.Model):
     ciudad = models.CharField(max_length=100)
     telefono = models.CharField(null=True, blank=True, max_length=20)
 
+def __str__(self):
+        texto = "{0},{1},{2},{3}"
+        return texto.format(self.nombre, self.direccion, self.ciudad, self.telefono)
 
+        
+def __str__(self):
+        texto = "{0}"
+        return texto.format(self.nombre)
