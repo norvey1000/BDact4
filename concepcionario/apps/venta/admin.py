8e5ae0f8480e855c9fffdd85dbbf3e1b0ea96297
norvey1000 <neramirez@uniguajira.edu.co>
2021-09-30 08:19:35 -0500

    relaciones entre coche y marca

--- concepcionario/apps/venta/admin.py
+++ concepcionario/apps/venta/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.db.models.query_utils import RegisterLookupMixin
-from apps.venta.models import Venta
 
-class VentaAdmin(admin.ModelAdmin):
-    list_display = ('CLIENTE', 'VEHICULO', 'fecha', 'total')
 # Register your models here.
 
-admin.site.register(Venta, VentaAdmin)
+
