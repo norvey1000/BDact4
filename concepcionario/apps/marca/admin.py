50a028eb55ded439f54694ed55e2a2275b8b26cf
hrodriguez21 <hrodriguezv@uniguijira.edu.co>
2021-09-30 08:42:21 -0500

    edicion de los archivos model y admin de la carpeta marca

--- concepcionario/apps/marca/admin.py
+++ concepcionario/apps/marca/admin.py
@@ -2,9 +2,17 @@ from apps import marca
 from django.contrib import admin
 from django.db.models.query_utils import RegisterLookupMixin
 
+from apps.marca.models import Marca
 
-
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('codigo_m', 'descripcion')
 # Register your models here.
+
+admin.site.register(Marca, MarcaAdmin)
+
+
+
+
 
 
 
--- concepcionario/apps/marca/models.py
+++ concepcionario/apps/marca/models.py
@@ -5,5 +5,9 @@ from django.db import models
 class Marca(models.Model):
     codigo_m = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=400)
+
+    def __str__(self):
+        texto = "{0},{1}"
+        return texto.format(self.codigo_m, self.descripcion)
     
   
